b5ffd72b2e45f1fbe71848d36d8320d20d692d2d
truhbro <47995263+truhbro@users.noreply.github.com>
2019-05-02 15:13:58 +1200

    v1 commit

---
+++ IGRAAM/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //Variables
+    public float movementSpeed; 
+
+    //Methods
+    void Update()
+    {
+        //Player Movement
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * movementSpeed * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * movementSpeed * Time.deltaTime);
+        }
+    }
+}
