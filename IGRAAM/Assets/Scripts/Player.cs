aca3ed96fd832200ebaf83dada625bad490ae670
JotaroJoestr <46461950+JotaroJoestr@users.noreply.github.com>
2019-05-10 12:57:33 +1200

    k

--- IGRAAM/Assets/Scripts/Player.cs
+++ IGRAAM/Assets/Scripts/Player.cs
@@ -11,19 +11,19 @@ public class Player : MonoBehaviour
     void Update()
     {
         //Player Movement
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.UpArrow))
         {
             transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
         }
-        if(Input.GetKey(KeyCode.A))
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Translate(Vector3.left * movementSpeed * Time.deltaTime);
         }
-        if(Input.GetKey(KeyCode.D))
+        if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
         }
-        if(Input.GetKey(KeyCode.S))
+        if(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
             transform.Translate(Vector3.back * movementSpeed * Time.deltaTime);
         }
